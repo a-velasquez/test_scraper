81de848737609529cae7af5ba28951db266a05d7
Alex Velasquez <jtinnesco@gmail.com>
2019-10-17 00:05:19 +0000

    added dependencies to test_scraper.rb

--- lib/test_scraper.rb
+++ lib/test_scraper.rb
@@ -1,3 +1,10 @@
+require 'open-uri'
+require 'nokogiri'
+require 'byebug'
+
+require "test_scraper/cli"
+require "test_scraper/article"
+require "test_scraper/scraper"
 require "test_scraper/version"
 
 module TestScraper
