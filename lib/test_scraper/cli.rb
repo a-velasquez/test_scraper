16ab9d0c50077325653b882d9f13ec8f17a054d8
Alex Velasquez <jtinnesco@gmail.com>
2019-10-17 06:34:02 +0000

    created recent_articles method

--- lib/test_scraper/cli.rb
+++ lib/test_scraper/cli.rb
@@ -7,7 +7,23 @@ class TestScraper::CLI
   end
 
   def greeting
-    puts "Loading ...".green.bold
+    puts "Loading ...............".green.bold
+    puts ""
+    puts "Welcome to:".green.bold
+    puts "--.--          |    ,---.                    |    ".green.bold
+    puts "  |  ,---.,---.|---.|    ,---..   .,---.,---.|---.".green.bold
+    puts "  |  |---'|    |   ||    |    |   ||   ||    |   |".green.bold
+    puts "  `  `---'`---'`   '`---'`    `---'`   '`---'`   '".green.bold
+    puts ""
+  end
+
+  def recent_articles
+    puts "Here's a list of the most recent articles:"
+    articles = TestScraper.scrape_tech_crunch
+    articles.each.with_index(1) do |value, index|
+      puts "#{index} - #{value.title}"
+      puts "#{value.url}"
+    end
   end
 
 
