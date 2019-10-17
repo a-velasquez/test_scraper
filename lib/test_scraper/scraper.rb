ad8a93a2a12a66fe077da40517de61c4115c59d1
Alex Velasquez <jtinnesco@gmail.com>
2019-10-17 05:14:09 +0000

    added to scrape_tech_crunch

--- lib/test_scraper/scraper.rb
+++ lib/test_scraper/scraper.rb
@@ -2,8 +2,15 @@ class TestScraper::Scraper
   attr_accessor :article_arr
 
   def self.scrape_tech_crunch
+    url = "https://techcrunch.com"
+    document = open(url)
+    content = document.read
+    parsed_content = Nokogiri::HTML(content)
 
-
+    parsed_content.css("h2.post-block__title").css("a").each.with_index(1) do |a, i|
+      title = a.children.text.strip
+      url = a.attribute("href").value
+    end
   end
 
 end
