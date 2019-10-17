c2673ae890f7560729ef42089e72d40b6f551114
Alex Velasquez <jtinnesco@gmail.com>
2019-10-17 06:42:56 +0000

    updated recent_article method

--- lib/test_scraper/cli.rb
+++ lib/test_scraper/cli.rb
@@ -10,20 +10,20 @@ class TestScraper::CLI
   def greeting
     puts "Loading ...............".green.bold
     puts ""
-    puts "Welcome to:".green.bold
-    puts "--.--          |    ,---.                    |    ".green.bold
-    puts "  |  ,---.,---.|---.|    ,---..   .,---.,---.|---.".green.bold
-    puts "  |  |---'|    |   ||    |    |   ||   ||    |   |".green.bold
-    puts "  `  `---'`---'`   '`---'`    `---'`   '`---'`   '".green.bold
+    puts "Welcome to:".white.bold
+    puts "--.--          |    ,---.                    |    ".white.bold
+    puts "  |  ,---.,---.|---.|    ,---..   .,---.,---.|---.".white.bold
+    puts "  |  |---'|    |   ||    |    |   ||   ||    |   |".white.bold
+    puts "  `  `---'`---'`   '`---'`    `---'`   '`---'`   '".white.bold
     puts ""
   end
 
   def recent_articles
-    puts "Here's a list of the most recent articles:"
+    puts "Here's a list of the most recent articles:".white.bold
     # articles = TestScraper.scrape_tech_crunch
     # articles.each.with_index(1) do |value, index|
-    puts "Article Title"
-    puts "Article URL"
+    puts "Article Title".white.bold
+    puts "Article URL".white.bold
   end
 
 end
