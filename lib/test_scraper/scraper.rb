eda01d5c5a000711c5481a1083e5a577748bf10c
Alex Velasquez <jtinnesco@gmail.com>
2019-10-22 02:24:48 +0000

    refactored self.scrape_tech_crunch

--- lib/test_scraper/scraper.rb
+++ lib/test_scraper/scraper.rb
@@ -1,20 +1,27 @@
 require 'nokogiri'
 require 'open-uri'
 
-class TestScraper::Scraper 
-  attr_accessor :article_arr
+class TestScraper::Scraper
+  attr_accessor :parsed_content
 
-  def self.scrape_tech_crunch
+  def initialize(url)
     url = "https://techcrunch.com"
     document = open(url)
     content = document.read
-    parsed_content = Nokogiri::HTML(content)
-    @article_arr = []
+    @parsed_content = Nokogiri::HTML(content)
 
-    parsed_content.css("h2.post-block__title").css("a").each do |element|
-      title = element.children.text.strip
-      url = element.attribute("href").value
+  end
+
+  def self.scrape_tech_crunch
+    articles = @parsed_content.css("h2.post-block__title").css("a")
+    top_stories = articles.map do |story|
+      stories = {
+        :title => sotry.children.text.strip
+        :url = story.attribute("href").value
+      }
+      TestScraper::Article.new(stories)
     end
+    byebug
   end
 
 end
