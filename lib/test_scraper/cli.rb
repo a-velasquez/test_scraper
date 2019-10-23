c34cfd05cb887c18baa75bf1367cfedb755b4ef9
Alex Velasquez <jtinnesco@gmail.com>
2019-10-23 06:20:55 +0000

    working on menu but everything works

--- lib/test_scraper/cli.rb
+++ lib/test_scraper/cli.rb
@@ -6,24 +6,48 @@ class TestScraper::CLI
     greeting
     TestScraper::Scraper.new("https://techcrunch.com").scrape_tech_crunch
     recent_articles
+    menu
   end
 
   def greeting
-    puts "Loading ...............".green.bold
+    puts "Loading ...............".green.bold.blink
     puts ""
-    puts "Welcome to:".white.bold
-    puts "--.--          |    ,---.                    |    ".white.bold
-    puts "  |  ,---.,---.|---.|    ,---..   .,---.,---.|---.".white.bold
-    puts "  |  |---'|    |   ||    |    |   ||   ||    |   |".white.bold
-    puts "  `  `---'`---'`   '`---'`    `---'`   '`---'`   '".white.bold
+    puts "Welcome to:".green.bold
+    puts "--.--          |    ,---.                    |    ".green.bold
+    puts "  |  ,---.,---.|---.|    ,---..   .,---.,---.|---.".green.bold
+    puts "  |  |---'|    |   ||    |    |   ||   ||    |   |".green.bold
+    puts "  `  `---'`---'`   '`---'`    `---'`   '`---'`   '".green.bold
     puts ""
   end
 
   def recent_articles
-    puts "Here's a list of the most recent articles:".white.bold
+    puts "Here's a list of the most recent articles:".green.bold
+    puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~".blue.bold
     TestScraper::Article.all.each.with_index(1) do |story, index|
-      puts "#{index}. #{story.title}"
-      puts "#{story.url}"
+      puts "#{index}. #{story.title}".blue.bold
+      puts "#{story.url}".blue.bold
+      puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    end
+  end
+
+  def menu
+    input = nil
+    while input != "exit"
+      puts "enter the number of the article you'd like to preview, or type 'recent' to see the list again".blue.bold
+      input = gets.strip.downcase
+
+      if input.to_i > 0
+        posting = TestScraper::Article.all[input.to_i-1]
+        puts "Here ya go!".blue.bold
+        puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~".blue.bold
+        puts "#{posting.title}"
+        puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~".blue.bold
+        puts "the full article can be found at: #{posting.url}"
+      elsif input == "recent"
+        recent_articles
+      else
+        puts "Hmmm. I didn't quite get that. Type 'recent' to see the lastest articles or 'exit.' to leave the application.".blue
+      end
     end
   end
 
