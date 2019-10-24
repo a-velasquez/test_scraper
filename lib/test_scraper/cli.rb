ec26391859aaa8d66c6bbbc3b4118bef8c9188eb
Alex Velasquez <jtinnesco@gmail.com>
2019-10-24 22:42:15 +0000

    reformatting menu

--- lib/test_scraper/cli.rb
+++ lib/test_scraper/cli.rb
@@ -8,38 +8,38 @@ class TestScraper::CLI
   end
 
   def greeting
-    puts "Loading ...............".blue.bold
-    puts ""
-    puts "Welcome to:".blue.bold
-    puts "--.--          |    ,---.                    |    ".blue.bold
-    puts "  |  ,---.,---.|---.|    ,---..   .,---.,---.|---.".blue.bold
-    puts "  |  |---'|    |   ||    |    |   ||   ||    |   |".blue.bold
-    puts "  `  `---'`---'`   '`---'`    `---'`   '`---'`   '".blue.bold
-    puts ""
+    puts "Loading ...............".colorize(:light_blue)
+    puts "".colorize(:light_blue)
+    puts "Welcome to:".colorize(:light_blue)
+    puts "--.--          |    ,---.                    |    ".colorize(:light_blue)
+    puts "  |  ,---.,---.|---.|    ,---..   .,---.,---.|---.".colorize(:light_blue)
+    puts "  |  |---'|    |   ||    |    |   ||   ||    |   |".colorize(:light_blue)
+    puts "  `  `---'`---'`   '`---'`    `---'`   '`---'`   '".colorize(:light_blue)
+    puts "".colorize(:light_blue)
   end
 
   def recent_articles
-    puts "Here's a list of the most recent articles:".blue.bold
-    puts "=".blue.bold * 100
+    puts "Here's a list of the most recent articles from Tech Crunch:".colorize(:light_blue)
+    puts "=".colorize(:light_blue) * 100
     TestScraper::Article.all.each.with_index(1) do |article, index|
-      puts ">> #{index}. #{article.title}".blue.bold
-      puts "=".blue.bold * 100
+      puts " ▶️ #{index}. #{article.title}".colorize(:light_blue)
+      puts "=".colorize(:light_blue) * 100
     end
   end
 
   def menu
     input = nil
     while input != "exit"
-      puts "Enter the number of the article you'd like to preview, or type 'recent' to see the list again".blue.bold
+      puts "Enter the number of the article you'd like to preview, or type 'recent' to see the list again".colorize(:light_blue)
       input = gets.strip
 
       if input.to_i > 0
         posting = TestScraper::Article.all[input.to_i-1]
-        puts "Here ya go!".blue.bold
-        puts "=".blue.bold * 100
-        puts ">> #{posting.preview}"
-        puts "=".blue.bold * 100
-        puts "the full article can be found at: #{posting.href}".blue.bold
+        puts "Here ya go!".colorize(:light_blue)
+        puts "=".colorize(:light_blue) * 100
+        puts ">> #{posting.preview}".colorize(:light_blue)
+        puts "=".colorize(:light_blue) * 100
+        puts "the full article can be found at: #{posting.href}".colorize(:light_blue)
       elsif input == "recent"
         recent_articles
       elsif input == "exit"
