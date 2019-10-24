793eb03b728b6f64dd5915878fe77852db66d94b
Alex Velasquez <jtinnesco@gmail.com>
2019-10-24 20:56:37 +0000

    cleaning up code

--- lib/test_scraper/cli.rb
+++ lib/test_scraper/cli.rb
@@ -10,44 +10,50 @@ class TestScraper::CLI
   end
 
   def greeting
-    puts "Loading ...............".green.bold.blink
+    puts "Loading ...............".blue.bold
     puts ""
-    puts "Welcome to:".green.bold
-    puts "--.--          |    ,---.                    |    ".green.bold
-    puts "  |  ,---.,---.|---.|    ,---..   .,---.,---.|---.".green.bold
-    puts "  |  |---'|    |   ||    |    |   ||   ||    |   |".green.bold
-    puts "  `  `---'`---'`   '`---'`    `---'`   '`---'`   '".green.bold
+    puts "Welcome to:".blue.bold
+    puts "--.--          |    ,---.                    |    ".blue.bold
+    puts "  |  ,---.,---.|---.|    ,---..   .,---.,---.|---.".blue.bold
+    puts "  |  |---'|    |   ||    |    |   ||   ||    |   |".blue.bold
+    puts "  `  `---'`---'`   '`---'`    `---'`   '`---'`   '".blue.bold
     puts ""
   end
 
   def recent_articles
-    puts "Here's a list of the most recent articles:".green.bold
-    puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~".blue.bold
-    TestScraper::Article.all.each.with_index(1) do |story, index|
-      puts "#{index}. #{story.title}".blue.bold
-      puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    puts "Here's a list of the most recent articles:".blue.bold
+    puts "=".blue.bold * 100
+    TestScraper::Article.all.each.with_index(1) do |article, index|
+      puts ">> #{index}. #{article.title}".blue.bold
+      puts "=".blue.bold * 100
     end
   end
 
   def menu
     input = nil
     while input != "exit"
-      puts "enter the number of the article you'd like to preview, or type 'recent' to see the list again".blue.bold
-      input = gets.strip.downcase
+      puts "Enter the number of the article you'd like to preview, or type 'recent' to see the list again".blue.bold
+      input = gets.strip
 
       if input.to_i > 0
         posting = TestScraper::Article.all[input.to_i-1]
         puts "Here ya go!".blue.bold
-        puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~".blue.bold
-        puts "#{posting.preview}"
-        puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~".blue.bold
-        puts "the full article can be found at: #{posting.href}"
+        puts "=".blue.bold * 100
+        puts ">> #{posting.preview}"
+        puts "=".blue.bold * 100
+        puts "the full article can be found at: #{posting.href}".blue.bold
       elsif input == "recent"
         recent_articles
+      elsif input == "exit"
+        goodbye
       else
         puts "Hmmm. I didn't quite get that. Type 'recent' to see the lastest articles or 'exit.' to leave the application.".blue
       end
     end
+  end
+
+  def goodbye
+    puts "Thanks For Stopping By!".blue.bold
   end
 
 end
--- lib/test_scraper/scraper.rb
+++ lib/test_scraper/scraper.rb
@@ -3,17 +3,15 @@ require 'open-uri'
 require 'pry'
 
 class TestScraper::Scraper
-  attr_accessor :doc, :articles, :title, :href, :preview
+  attr_accessor :doc
 
   def initialize(url)
     url = "https://techcrunch.com"
     @doc = Nokogiri::HTML(open(url))
   end
 
-#working - now just have to find a  way to call the information to CLI
-
   def scrape_tech_crunch
-    @doc.css('.post-block').map do |story| # navigate down from the selected node
+    @doc.css('.post-block').map do |story| # navigates down from the selected node
 
     title   = story.at_css('h2.post-block__title a')
     preview = story.at_css('div.post-block__content')
@@ -24,29 +22,5 @@ class TestScraper::Scraper
       preview: preview.content.strip
       )
     end
-    binding.pry
   end
-
-
 end
-
-# def scrape_tech_crunch
-#   articles = @doc.css("h2.post-block__title").css("a")
-#   top_stories = articles.each do |story|
-#     stories = {
-#       :title => story.children.text.strip,
-#       :url => story.attribute("href").value,
-#       :preview => @doc.css("div.post-block__content").children.first.text #only brings up the first article's preview
-#     }
-#     TestScraper::Article.new(stories)
-#   end
-# end
-
-# :preview css selector for iterator => a = doc.css("div.post-block__content").children.text
-
-# def list_articles
-#   TestScraper::Article.all.each.with_index(1) do |story, index|
-#     puts "#{index}. #{story.title}"
-#     puts "#{story.url}"
-#   end
-# end
